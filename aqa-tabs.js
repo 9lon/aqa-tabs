912417d1c5f6397d76b886849e3ee8b4350b4f6f
Tonsai jompun <tonsai.j@nexts-corp.com>
2018-06-05 13:50:37 +0700

    update @nylon

--- aqa-tabs.js
+++ aqa-tabs.js
@@ -1,6 +1,6 @@
 import { PolymerElement } from '@polymer/polymer/polymer-element.js';
-// import '@aqa/aqa-font/aqa-font.js';
-import '@aqa/aqa-font/aqa-mitr-font.js';
+// import '@nylon/aqa-font/aqa-font.js';
+import '@nylon/aqa-font/aqa-mitr-font.js';
 
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 /**
